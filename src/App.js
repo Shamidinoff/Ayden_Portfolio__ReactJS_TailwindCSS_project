0e078148f5dbc52d639d73c1fd2a3cde4e93449a
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-04 19:39:14 +0600

    Ayden Portfolio - Intro with animation Banner.JS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 // components
-import Banner from './components/Banner';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import About from './components/About';
-import Services from './components/Services';
-import Work from './components/Work';
-import Contact from './components/Contact';
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Services from "./components/Services";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
       <Header />
       <Banner />
       <Nav />
@@ -18,7 +18,7 @@ const App = () => {
       <Services />
       <Work />
       <Contact />
-      {/* <div className='h-[4000px]'></div> */}
+      <div className="h-[4000px]"></div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import Logo from "../assets/logo.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* logo */}
+          <a href="#">
+            <img src={Logo} alt="BEN AYDEN logo" />
+          </a>
+          {/* button */}
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
