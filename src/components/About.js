e1c4b26a8a26b257cce2714f13b50f081764eac2
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-04 19:42:15 +0600

    Ayden Portfolio - Intro with animation Banner.JS

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const About = () => {
   return (
-    <div className="section" id="about">
+    <section className="section" id="about">
       About
-    </div>
+    </section>
   );
 };
 
