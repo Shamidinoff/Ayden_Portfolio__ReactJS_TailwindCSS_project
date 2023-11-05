d4f1f0d0b2cef58cca0531ceaa77fe2c8ce3ddc1
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-05 12:49:00 +0600

    Ayden Portfolio - Work section is Done

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,8 +9,14 @@ const Work = () => {
   return (
     <section className="section" id="work">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row">
-          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
+          >
             {/* text */}
             <div>
               <h2 className="h2 leading-tight text-accent">
@@ -19,9 +25,7 @@ const Work = () => {
               </h2>
               <p className="max-w-sm mb-16">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Dolores, aliquam! Corporis maiores repellat autem qui, odio
-                veritatis consequuntur quae eaque beatae nostrum dolor accusamus
-                aliquid repudiandae quam cupiditate id voluptate.
+                Dolores, aliquam! Corporis maiores repellat autem qui.
               </p>
               <button className="btn btn-sm">View all projects</button>
             </div>
@@ -44,9 +48,15 @@ const Work = () => {
                 <span className="text-3xl text-white">Project Title</span>
               </div>
             </div>
-          </div>
+          </motion.div>
           {/* вторая колонка */}
-          <div className="flex-1 flex flex-col gap-y-10">
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-10"
+          >
             {/* image */}
             <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
               {/* overlay */}
@@ -86,7 +96,7 @@ const Work = () => {
                 <span className="text-3xl text-white">Project Title</span>
               </div>
             </div>
-          </div>
+          </motion.div>
           {/* конец */}
         </div>
       </div>
